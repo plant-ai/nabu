78e81181ae70ea2ea5dba791ad63b4622f5522cc
plant-ai <149395424+plant-ai@users.noreply.github.com>
2025-08-08 14:38:57 +0100

    Delete nabu-iteration-2.5/nabu/util/case_crypto.py
    not needed

--- nabu-iteration-2.5/nabu/util/case_crypto.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-def encrypt(cipher, plainText: str, alphabet: str = "latin", preserve: bool = True) -> str:
-    return cipher.encrypt(plainText)
-
-def decrypt(cipher, cipherText: str, alphabet: str = "latin", preserve: bool = True) -> str:
-    return cipher.decrypt(cipherText)
